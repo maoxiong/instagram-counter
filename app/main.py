cca0fcc59f9f364644857e4e2c1da963b12f0391
Ben Griffiths <ben@griffiths.me.uk>
2025-12-26 19:50:00 +0800

    v2 - With settings option (cookies) which will override defaults.
    UNLESS - Env variables will always set as main (and ignore cookies)

--- app/main.py
+++ app/main.py
@@ -14,39 +14,76 @@ load_dotenv()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+defaults = {
+    "INSTAGRAM_USERNAME": "softcatmemes",
+    "REFRESH_INTERVAL": 10,
+    "MINIMUM_DIGITS": 5,
+    "PAD_CHARACTER": "0",
+    "FONT_SIZE": "4",
+    "FONT_FAMILY": "",
+    "PAGE_BG": "#000000",
+    "FLIP_BG": "#333232",
+    "FLIP_FG": "#edebeb",
+    "SHOW_IG_LOGO": "1",
+    "SKIP_ANIMATION": "0",
+}
+
+# So that instagram doesn't block your scraping IP
+minimum_refresh_interval = 5
+
+def get_var(request: Request, key):
+    if os.getenv("INSTAGRAM_USERNAME"):
+        return os.getenv(key) or defaults[key]
+    else:
+        return request.cookies.get(key) or defaults[key]
+
+
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    interval = max(5, int(os.getenv("REFRESH_INTERVAL", 10)))
+    interval = max(minimum_refresh_interval, int(get_var(request, "REFRESH_INTERVAL")))
 
-    padding = os.getenv("PADDING", "00000")
-    transform = f"arrive(.2) -&gt; round -&gt; pad('{ padding }') -&gt; split -&gt; delay(rtl, 100, 150)" if os.getenv("SKIP_ANIMATION", "0") == "0" else f"pad('{ padding }')"
+    pad_character = get_var(request, "PAD_CHARACTER")
+    digits = int(get_var(request, "MINIMUM_DIGITS"))
 
-    start_value = followers().get("followers", 0)
+    padding = pad_character * digits
+    skip_animation = get_var(request, "SKIP_ANIMATION") == "1"
+    transform = f"arrive(.2) -&gt; round -&gt; pad('{ padding }') -&gt; split -&gt; delay(rtl, 100, 150)" if not skip_animation else f"pad('{ padding }')"
+
+    start_value = followers(request).get("followers", 0)
     # if the start value is divisible by 10, subtract 10 from it so that there will be a change on boot up
     if start_value > 0 and start_value % 10 == 0:
         start_value -= 10
     # round down to the nearest 10 (giving us a nice animation run-up)
     start_value = (start_value // 10) * 10
 
+    settings_enabled = bool(os.getenv("INSTAGRAM_USERNAME") or request.cookies.get("LOCK_SETTINGS") == "1") is False
+
     return templates.TemplateResponse(
         "home.html",
         {
             "request": request,  # required
-            "instagram_username": os.getenv("INSTAGRAM_USERNAME", "softcatmemes"),
-            "refresh_interval": interval * 60000,
+            "instagram_username": get_var(request,"INSTAGRAM_USERNAME"),
+            "refresh_interval": interval,
+            "minimum_refresh_interval": minimum_refresh_interval,
             "start_value": start_value,
             "transform": transform,
-            "font_size": os.getenv("FONT_SIZE", "4"),
-            "font_family": os.getenv("FONT_FAMILY", ""),
-            "page_bg": os.getenv("PAGE_BG", "#000000"),
-            "flip_bg": os.getenv("FLIP_BG", "#333232"),
-            "flip_fg": os.getenv("FLIP_FG", "#edebeb"),
-            "ig_logo": os.getenv("IG_LOGO", "1"),
+            "font_size": get_var(request,"FONT_SIZE"),
+            "font_family": get_var(request,"FONT_FAMILY"),
+            "page_bg": get_var(request,"PAGE_BG"),
+            "flip_bg": get_var(request,"FLIP_BG"),
+            "flip_fg": get_var(request,"FLIP_FG"),
+            "show_ig_logo": get_var(request,"SHOW_IG_LOGO") == "1",
+            "skip_animation": skip_animation,
+            "pad_character": pad_character,
+            "minimum_digits": digits,
+            "defaults": defaults,
+            "settings_enabled": settings_enabled
         }
     )
 
 @app.get("/api/followers")
-def followers():
+def followers(request: Request):
+
     url = "https://i.instagram.com/api/v1/users/web_profile_info/"
 
     headers = {
@@ -54,7 +91,7 @@ def followers():
     }
 
     try:
-        resp = requests.get(url, headers=headers, params={"username": os.getenv("INSTAGRAM_USERNAME", "softcatmemes")})
+        resp = requests.get(url, headers=headers, params={"username": get_var(request, "INSTAGRAM_USERNAME")})
         resp.raise_for_status()         # raises error on 4xx/5xx
 
         json_data = resp.json()
